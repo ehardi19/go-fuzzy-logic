24c89c94bf175ebb2bc987e2a8ed0b9b6455ee7b
Eko Hardiyanto <ehardi19@gmail.com>
2019-04-19 06:31:15 +0700

    Imlement main app

--- app/main.go
+++ app/main.go
@@ -1,7 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/ehardi19/go-fuzzy-logic"
+)
 
 func main() {
-	fmt.Println("hello world")
+	csvFile, _ := os.Open("DataTugas3.csv")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	defer csvFile.Close()
+	var data []fuzzy.Number
+	reader.Read()
+	for {
+		var fn fuzzy.Number
+		row, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fn.Interview.ID = row[0]
+		fn.Interview.Competence, err = strconv.ParseFloat(row[1], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fn.Interview.Personality, err = strconv.ParseFloat(row[2], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		data = append(data, fn)
+	}
+
+	file, _ := os.Create("TebakanTugas3.csv")
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Fuzzy
+	blt := fuzzy.BLT{}
+	for i := range data {
+		blt.Fuzzification(&data[i])
+		blt.Inference(&data[i])
+		blt.Defuzzification(&data[i])
+	}
+
+	// Inseting Data
+	head := []string{
+		"ID",
+		"Competence",
+		"Personality",
+		"Diterima",
+	}
+	if err := writer.Write(head); err != nil {
+		log.Fatalln("error writing record to csv:", err)
+	}
+	for i := range data {
+		csvData := []string{
+			fmt.Sprintf("%s", data[i].Interview.ID),
+			fmt.Sprintf("%.0f", data[i].Interview.Competence),
+			fmt.Sprintf("%.0f", data[i].Interview.Personality),
+			fmt.Sprintf("%f", data[i].CrispValue),
+		}
+		if err := writer.Write(csvData); err != nil {
+			log.Fatalln("error writing record to csv:", err)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ require (
 	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/jung-kurt/gofpdf v1.1.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20190321074620-2f0d2b0e0001 // indirect
-	golang.org/x/crypto v0.0.0-20190417174047-f416ebab96af // indirect
+	golang.org/x/crypto v0.0.0-20190418165655-df01cb2cc480 // indirect
 	golang.org/x/exp v0.0.0-20190417140011-e40e924fdd3f // indirect
 	golang.org/x/image v0.0.0-20190417020941-4e30a6eb7d9a // indirect
 	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
 	golang.org/x/net v0.0.0-20190415214537-1da14a5a36f2 // indirect
-	golang.org/x/sys v0.0.0-20190416152802-12500544f89f // indirect
-	golang.org/x/tools v0.0.0-20190417223002-a5870b403859 // indirect
+	golang.org/x/sys v0.0.0-20190418153312-f0ce4c0180be // indirect
+	golang.org/x/tools v0.0.0-20190418152851-6732636ccdfd // indirect
 	gonum.org/v1/gonum v0.0.0-20190418073911-7c4bc5eb9955 // indirect
 	gonum.org/v1/netlib v0.0.0-20190331212654-76723241ea4e // indirect
 	gonum.org/v1/plot v0.0.0-20190410204940-3a5f52653745 // indirect
